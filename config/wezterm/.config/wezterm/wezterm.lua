410e0838c961083ed1cf20d38fef3d04e6016862
skyeBreach <skyebenson@hotmail.com>
2025-04-20 18:48:13 +0100

    feat: add wezterm config files

---
+++ config/wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- 
+config.color_scheme = 'Catppuccin Macchiato'
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+
+-- and finally, return the configuration to wezterm
+return config
